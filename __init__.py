134de9604b9863b9ba9ba3c27775e70bef401ba6
keelan542 <keelanb542@gmail.com>
2023-10-30 13:36:57 +0000

    function to get energies from forward and reverse

--- __init__.py
+++ __init__.py
@@ -2,11 +2,26 @@
 ircr_file = input("Enter name of Reverse IRC log file: ")
 ircf_file = input("Enter name of Forward IRC log file: ")
 
-# Define lists to store energies
+
+def get_energies(filename, is_reverse=False):
+    energies = []
+
+    with open(filename) as file:
+        for line in file:
+            if "Corrected End Point Energy" in line:
+                energies.append(float(line.split()[5]))
+
+    if is_reverse:
+        return energies.reverse()
+    else:
+        return energies
+
 
 # Get reverse energies (reverse order)
+reverse_energies = get_energies(ircr_file, is_reverse=True)
 
 # Get forward energies
+forward_energies = get_energies(ircf_file)
 
 # Combine reverse and forward energies into total energies
 
