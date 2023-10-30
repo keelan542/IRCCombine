86e03601863e8e00f95abebb6cde316d3d449cea
keelan542 <keelanb542@gmail.com>
2023-10-30 13:26:22 +0000

    prompt user for name of irc files

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# Prompt user for required information --> name of reverse and forward irc log files
+ircr_file = input("Enter name of Reverse IRC log file: ")
+ircf_file = input("Enter name of Forward IRC log file: ")
+
+# Define lists to store energies
+
+# Get reverse energies (reverse order)
+
+# Get forward energies
+
+# Combine reverse and forward energies into total energies
+
+# Create new relative energy list for path (using first point as reference)
